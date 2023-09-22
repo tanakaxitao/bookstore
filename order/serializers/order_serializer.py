7a54710f95643c171ee30d160512bc13f8947b03
tanakaxitao <tanakacvr@gmail.com>
2023-09-22 13:12:16 -0400

    tests in viewsets

--- order/serializers/order_serializer.py
+++ order/serializers/order_serializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from order.models import Order
 from product.models import Product
 from product.serializers.product_serializer import ProductSerializer
 
@@ -15,3 +16,13 @@ class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ['product', 'total']
+
+    def create(self, validated_data):
+        product_data = validated_data.pop("products_id")
+        user_data = validated_data.pop("user")
+
+        order = Order.objects.create(user=user_data)
+        for product in product_data:
+            order.product.add(product)
+
+        return order
---
+++ order/viewsets/__init__.py
@@ -0,0 +1,5 @@
+from .order_viewset import OrderViewSet
+
+
+
+
