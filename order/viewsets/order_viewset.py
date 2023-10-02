12fe44ebf1b4ca9bc91db681b3bec26281eba64b
tanakaxitao <tanakacvr@gmail.com>
2023-10-02 17:39:33 -0400

    create Dockerfile

--- order/viewsets/order_viewset.py
+++ order/viewsets/order_viewset.py
@@ -1,6 +1,4 @@
-from rest_framework import status
-from rest_framework.mixins import CreateModelMixin
-from rest_framework.response import Response
+
 from rest_framework.viewsets import ModelViewSet
 
 from order.models import Order
@@ -10,4 +8,4 @@ from order.serializers import OrderSerializer
 class OrderViewSet(ModelViewSet):
 
     serializer_class = OrderSerializer
-    queryset = Order.objects.all()
+    queryset = Order.objects.all().order_by("id")
